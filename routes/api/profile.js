50a7d480049a882df895ba5d11759183084e3ebb
“leonzlotnik” <zlotnik.leon91@gmail.com>
2019-05-15 23:02:42 -0500

    [ADD] profile routes y model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express();
+const auth = require('../../middleware/auth');
 
-router.get('/', (req,res) =>{
-    res.send('Router de perfil');
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
+
+router.get('/me', auth, async (req,res) =>{
+    try{
+        const profile = await Profile.findOne({user : req.user.id}).populate('user',
+        ['name','avatar']);
+
+        if(!profile){
+            return res.status(400).json({msg: "No hay perfil para este usuario"});
+        }
+    }catch(err){
+        console.log(err)
+        res.status(500).json({msg:"Error de Servidor"})
+    }
 });
 
 module.exports = router;
