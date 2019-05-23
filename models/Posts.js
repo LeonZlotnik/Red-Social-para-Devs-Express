03375dafbd119bf0c03a30eab019c1aac303cf5b
“leonzlotnik” <zlotnik.leon91@gmail.com>
2019-05-23 11:37:49 -0500

    [ADD] model Posts.js y extend´i CRUD en profile

---
+++ models/Posts.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema ({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    text:{
+        type: String,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    avatar:{
+        type: String
+    },
+    likes: [
+        {
+            user:{
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        }
+    ],
+    comments: [
+        {
+            user:{
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            },
+            text:{
+                type: String,
+                required: true
+            },
+            avatar:{
+                type: String
+            },
+            date:{
+                type: Date,
+                dafault: Date.now
+            }
+        }
+    ],
+    date:{
+        type: Date,
+        defaule: Date.now
+    }
+});
+
+Post = mongoose.model('post', PostSchema);
+
+module.exports = Post;
